f933e9be0666241e091fc6640605750f1ea8fc18
Carlos-code36 <cacharfuelan@umariana.edu.co>
2021-04-02 17:50:55 -0500

    Archivo nominado Numero de modifico el tipo de mensaje que sera impreso en pantalla

---
+++ Condicional1.py
@@ -0,0 +1,9 @@
+num1= int(input ("Ingrese primer numero: "))
+num2 = int(input("Ingrese segundo numero: ")) 
+if num1>num2:
+    print("¨{} Es mayor que {}".format(num1,num2))
+else:
+    if(num2>num1):
+        print("¨{} Es mayor que {}".format(num2,num1))
+    else:
+        ("¨{} Es igual que {}".format(num2,num1))
---
+++ import math.py
@@ -0,0 +1,4 @@
+
+print("Calcular Figuras Geométricas.\n")
+print("1.Cuadrado.\n2.Rectangulo.\n3.Triángulo.\n4.Circulo.\n5.Rombo.\n6.Trapecio.\n7.Paralelogramo.\n8.Elipse.\n9.Pentágono\n")
+
